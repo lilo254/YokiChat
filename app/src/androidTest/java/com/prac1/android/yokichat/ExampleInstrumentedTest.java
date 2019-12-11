60a63c48b9824e1655a17611249edd9a01014046
lilo254 <34243951+lilo254@users.noreply.github.com>
2019-12-11 22:33:53 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/prac1/android/yokichat/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.prac1.android.yokichat;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.prac1.android.yokitchat", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/prac1/android/yokichat/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.prac1.android.yokichat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    // Firebase Auth
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // Android fields
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
+        mCreateBtn = (Button) findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                register_user(display_name, email, password);
+
+            }
+        });
+
+    }
+
+    private void register_user(String display_name, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+
+                } else {
+
+                    Toast.makeText(RegisterActivity.this,"you got some error.", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+}
